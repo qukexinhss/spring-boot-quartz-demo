dafe06838b6522684b7e68d372712f0645dc5d53
qkx <qkx@192.168.1.112>
2017-09-16 01:44:21 +0800

    修复bug。
    Change-Id: I32481fca85b2613c81e1c685679c55bd8e0f83e7

--- src/main/java/com/kaviddiss/bootquartz/Application.java
+++ src/main/java/com/kaviddiss/bootquartz/Application.java
@@ -11,6 +11,6 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        
+        System.out.println("Spring-boot-quartz-demo start successful.");
     }
 }
